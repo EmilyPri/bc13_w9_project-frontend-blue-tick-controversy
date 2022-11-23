bf278e1828de151dff541bc065ec872e909ead63
omarcodes98 <omarali_15@hotmail.co.uk>
2022-11-23 09:51:53 +0000

    fixed dropdowns

--- bootcamp-links-library/src/Components/Dropdown/Dropdown.js
+++ bootcamp-links-library/src/Components/Dropdown/Dropdown.js
@@ -2,17 +2,24 @@ import React from "react";
 
 function Dropdown(props) {
   return [
-    <input
-      className={props.className}
-      onChange={props.handleChange}
-      list={props.list}
-      name={props.name}
-      placeholder={props.placeholder}
-    />,
+    // <input
+    //   className={props.className}
+    //   onChange={props.handleChange}
+    //   list={props.list}
+    //   name={props.name}
+    //   placeholder={props.placeholder}
+    // />,
 
-    <datalist id={props.dataId}>
-      <option key={props.key1} value={props.value1}/>
-    </datalist>,
+    <select id={props.dataId} className={props.className} onChange={props.handleChange} placeholder={props.placeholder}>
+      <option value={props.value1}>{props.text1}</option>
+      <option value={props.value2}>{props.text2}</option>
+      <option value={props.value3}>{props.text3}</option>
+      <option value={props.value4}>{props.text4}</option>
+      <option value={props.value5}>{props.text5}</option>
+      <option value={props.value6}>{props.text6}</option>
+      <option value={props.value7}>{props.text7}</option>
+      <option value={props.value8}>{props.text8}</option>
+    </select>,
   ];
 }
 
