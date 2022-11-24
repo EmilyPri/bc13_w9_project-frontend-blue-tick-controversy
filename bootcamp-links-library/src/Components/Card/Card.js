673be3f027ae47b496bf2a2371dcad0d96b5f246
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2022-11-24 12:30:40 +0000

    likes button css

--- bootcamp-links-library/src/Components/Card/Card.js
+++ bootcamp-links-library/src/Components/Card/Card.js
@@ -4,17 +4,27 @@ function Card(props) {
   return [
     <div className="card-container">
       <div className="card-top">
-        <div><img src={props.subjectIcon} /></div>
+        <div>
+          <img src={props.subjectIcon} />
+        </div>
         <h1>{props.title}</h1>
       </div>
-      
+
       <div className="card-middle">
-        <button className="card-button" onClick={props.buttonCopy}>Copy to clipboard</button>
-        <button className="card-button" onClick={props.buttonLink}>Take me there</button>
+        <button className="card-button" onClick={props.buttonCopy}>
+          Copy to clipboard
+        </button>
+        <button className="card-button" onClick={props.buttonLink}>
+          Take me there
+        </button>
       </div>
       <div className="card-bottom">
-        <p>{props.description}</p>
-        {/* <button onClick={props.handleLike}>Likes : {props.numLikes}</button> */}
+        <div className="p-description-container">
+          <p>{props.description}</p>
+        </div>
+        <div className="like-container">
+          <button onClick={props.handleLike}>❤️ {props.numLikes}</button>
+        </div>
       </div>
     </div>,
   ];
