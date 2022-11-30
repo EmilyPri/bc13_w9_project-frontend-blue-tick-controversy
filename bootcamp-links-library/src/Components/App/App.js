8906144353ec5bf4da51fc4fa41e753955c7d0c2
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2022-11-30 14:04:02 +0000

    fixing problems wit commits

--- bootcamp-links-library/src/Components/App/App.js
+++ bootcamp-links-library/src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import Banner from "../Banner/Banner";
 import Button from "../Button/Button";
--- bootcamp-links-library/src/Components/DropdownSubject/DropdownSubject.js
+++ bootcamp-links-library/src/Components/DropdownSubject/DropdownSubject.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-function Dropdown(props) {
+function DropdownSubject(props) {
   return [
      <select id={props.dataId} className={props.className} onChange={props.handleChange} placeholder={props.placeholder} list={props.list}>
       <option value="">{props.placeholder}</option>
-      <option value="API">API</option>
-      <option value="CSS">CSS</option>
-      <option value="JavaScript">JavaScript</option>
-      <option value="React">React</option>
-      <option value="HTML">HTML</option>
-      <option value="General Dev">General Dev</option>
-      <option value="SQL">SQL</option>
-      <option value="Git">Git</option>
+      <option value="1">API</option>
+      <option value="2">CSS</option>
+      <option value="3">JavaScript</option>
+      <option value="4">React</option>
+      <option value="5">HTML</option>
+      <option value="6">General Dev</option>
+      <option value="7">SQL</option>
+      <option value="8">Git</option>
     </select>,
   ];
 }
 
-export default Dropdown;
+export default DropdownSubject;
